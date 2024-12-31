2c3c0e8616b9cc610be9f5c36edf0232ce46ca62
Tarik Fayad <tarikfayad@gmail.com>
2024-12-31 20:04:46 +0200

    Initial commit for Restart.

---
+++ Restart/Restart-App/Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Tarik Fayad on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
