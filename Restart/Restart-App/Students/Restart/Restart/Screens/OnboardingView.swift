f0ca8d97e27f8ba08e1a2b7887aac9fb3070a7d4
Tarik Fayad <tarikfayad@gmail.com>
2025-01-02 15:02:45 +0200

    OnboradingView Work
    1. Built out the UI elements for the onboarding view.
    2. Added a a tap gesture to the custom slider view at the bottom of the screen to tweak the value of the isOnboardingViewActive variable in user defaults.

--- Restart/Restart-App/Students/Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart-App/Students/Restart/Restart/Screens/OnboardingView.swift
@@ -11,16 +11,96 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true // If the key already exists in user defaults it shouldn't overwrite the value.
     
     var body: some View {
-        VStack (spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
             
-            Button {
-                isOnboardingViewActive = false
-            } label: {
-                Text("Start")
-            }
-        } //: VSTACK
+            VStack (spacing: 20) {
+                // MARK: - HEADER
+                Spacer()
+                VStack(spacing: 20) {
+                    Text("Share.")
+                        .font(.system(size: 60, weight: .heavy))
+                        .foregroundStyle(.white)
+                    
+                    Text("It's not how much we give but\nhow much love we put into giving.")
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                }
+                
+                // MARK: - BODY
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, alignment: .center)
+                        
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, alignment: .center)
+                    } //: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: BODY
+                
+                // MARK: - FOOTER
+                ZStack {
+                    // Background
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // Call to action
+                    Text("Get Started")
+                        .font(.title3)
+                        .fontDesign(.rounded)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .offset(x: 20)
+                    
+                    // Capsule
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // Draggable Circle
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+
+                            Circle()
+                                .fill(.black.opacity(0.2))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundStyle(.white)
+                        .frame(width: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
