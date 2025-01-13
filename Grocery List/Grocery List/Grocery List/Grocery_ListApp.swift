90e3316bc4bbcc6c19ce5f8a55a8cc786fd443af
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-13 11:47:20 -0500

    Created the Grocery List application.

---
+++ Grocery List/Grocery List/Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Tarik Fayad on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
