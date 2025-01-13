dd437a0a0217a839a1efdf161cb1ead25844b535
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-13 10:45:58 -0500

    Ground work for SwiftData connection to SwiftUI
    Imported SwiftData into all scenes, added query and environments to ContentView.

--- Wishlist/Wishlist-App/Students/Wishlist/Wishlist/ContentView.swift
+++ Wishlist/Wishlist-App/Students/Wishlist/Wishlist/ContentView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var wishes: [Wish]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
