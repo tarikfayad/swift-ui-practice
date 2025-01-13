e3276c355c216bedd6e9d6cddc38cb52a5c24468
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-13 11:36:07 -0500

    Added the ability to delete wishes.

--- Wishlist/Wishlist-App/Students/Wishlist/Wishlist/ContentView.swift
+++ Wishlist/Wishlist-App/Students/Wishlist/Wishlist/ContentView.swift
@@ -22,6 +22,11 @@ struct ContentView: View {
                     Text(wish.title)
                         .font(.title.weight(.light))
                         .padding(.vertical, 2)
+                        .swipeActions {
+                            Button("Delete", role: .destructive) {
+                                modelContext.delete(wish)
+                            }
+                        }
                 }
             }
             .navigationTitle("Wishlist")
